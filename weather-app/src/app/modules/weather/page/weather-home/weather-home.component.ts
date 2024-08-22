11253db1b07569601a539c53a8afd69de0f34e82
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-08-22 20:46:01 -0300

    Criação do serviço openWeatherMap para consumo dos dados da API

--- weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject<void>();
 
+  initialCityName: string = 'Sao Paulo';
+  weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  public getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log("dados: ", this.weatherDatas);
+
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  public onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- weather-app/src/app/modules/weather/services/weather.service.ts
+++ weather-app/src/app/modules/weather/services/weather.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private apiKey = '5574751c01fa6cfce3f57030294709a4';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getWeatherDatas(cityName: string): Observable<any> {
+    return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metrics&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
 }
