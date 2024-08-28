7108be06a5a01704f6f3d1b2f99e196cb2d34c9b
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-08-27 22:07:27 -0300

    fix: ocultar chave api

--- weather-app/src/app/environment.ts
+++ weather-app/src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.openweathermap.org/data/2.5/',
-  apiKey: '5574751c01fa6cfce3f57030294709a4',
+  apiKey: 'SUA_CHAVE_API_AQUI',
 };
